dfae7f1673099d5a97656297f73c45e6e6d066da
Aleksey Kononenko <aleksey19988@gmail.com>
2021-10-06 15:14:33 +0300

    Добавил шаблонизатор twig. Реализовал отрисовку главной страницы и страницы about

--- index.php
+++ index.php
@@ -2,20 +2,29 @@
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
+use Twig\Environment;
+use Twig\Loader\FilesystemLoader;
 
 require __DIR__ . '/vendor/autoload.php';
+
+$loader = new FilesystemLoader('templates');
+$view = new Environment($loader);
 
 $app = AppFactory::create();
 $app->setBasePath("/my-php-blog");
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("General page!");
+$app->get('/', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('index.twig');
+    $response->getBody()->write($body);
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) {
-    $response->getBody()->write("About page!");
+$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+    $body = $view->render('about.twig', [
+        'name' => 'Alex',
+    ]);
+    $response->getBody()->write($body);
     return $response;
 });
 
