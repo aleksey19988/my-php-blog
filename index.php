823201142d54e2986aebc62785828d7152a18254
Aleksey Kononenko <aleksey19988@gmail.com>
2021-10-07 11:17:02 +0300

    добавил БД, таблицу, запросы к таблице для отрисовки постов. Также добавил pos mapper, реализовал это в index.php. Добавил страницу not-found в случае, если по ссылке не найден пост

--- index.php
+++ index.php
@@ -4,11 +4,29 @@ use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
 use Twig\Environment;
 use Twig\Loader\FilesystemLoader;
+use Aleksey\MyPhpBlog\PostMapper;
 
 require __DIR__ . '/vendor/autoload.php';
 
 $loader = new FilesystemLoader('templates');
 $view = new Environment($loader);
+
+$config = include 'config/database.php';
+//$dsn = $config['dsn'];
+//$username = $config['username'];
+//$password = $config['password'];
+['dsn' => $dsn, 'username' => $username, 'password' => $password] = $config;
+
+try {
+    $connection = new PDO($dsn, $username, $password);
+    $connection->setAttribute(PDO::ATTR_ERRMODE, PDO::ERRMODE_EXCEPTION);
+    $connection->setAttribute(PDO::ATTR_DEFAULT_FETCH_MODE, PDO::FETCH_ASSOC);
+} catch (PDOException $exception) {
+    echo 'Database error: ' . $exception->getMessage();
+    die();
+}
+
+$postMapper = new PostMapper($connection);
 
 $app = AppFactory::create();
 $app->setBasePath("/my-php-blog");
@@ -28,10 +46,15 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view) {
-    $body = $view->render('post.twig', [
-        'url_key' => $args['url_key'],
-    ]);
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+    $post = $postMapper->getByUrlKey((string) $args['url_key']);
+    if (empty($post)) {
+        $body = $view->render('not-found.twig');
+    } else {
+        $body = $view->render('post.twig', [
+            'post' => $post,
+        ]);
+    }
     $response->getBody()->write($body);
     return $response;
 });
---
+++ src/PostMapper.php
@@ -0,0 +1,36 @@
+<?php
+
+namespace Aleksey\MyPhpBlog;
+
+use PDO;
+
+class PostMapper
+{
+    /**
+     * @var PDO
+     */
+    private PDO $connection;
+
+    /**
+     * @param PDO $connection
+     */
+    public function __construct(PDO $connection)
+    {
+        $this->connection = $connection;
+    }
+
+    /**
+     * @param string $urlKey
+     * @return array|null
+     */
+    public function getByUrlKey(string $urlKey): ?array
+    {
+        $statement = $this->connection->prepare('SELECT * FROM post WHERE url_key = :url_key');
+        $statement->execute([
+            'url_key' => $urlKey,
+        ]);
+        $result = $statement->fetchAll();
+
+        return array_shift($result);
+    }
+}
