acf0a9f083b01262728e71e8716b82e462f8c70c
Aleksey Kononenko <aleksey19988@gmail.com>
2021-10-08 15:34:24 +0300

    Реализовал отдельный класс с методами для доступа к последним постам (с возможностью выбрать кол-во постов). Также в файле index.php изменил логику создания объекта PostMapper и перенёс создание объекта только в те ситуации, где это нужно, т. е. раньше объект создавался вне какого-либо перехода по url, сейчас он создаётся только при переходе по тому url, когда он нужен будет для дальнейшей отрисовки

--- index.php
+++ index.php
@@ -1,4 +1,6 @@
 <?php
+
+use Aleksey\MyPhpBlog\LatestPosts;
 use Psr\Http\Message\ResponseInterface as Response;
 use Psr\Http\Message\ServerRequestInterface as Request;
 use Slim\Factory\AppFactory;
@@ -26,14 +28,13 @@ try {
     die();
 }
 
-$postMapper = new PostMapper($connection);
-
 $app = AppFactory::create();
 $app->setBasePath("/my-php-blog");
 $app->addErrorMiddleware(true, true, true);
 
-$app->get('/', function (Request $request, Response $response, $args) use ($view, $postMapper) {
-    $posts = $postMapper->getList('DESC');
+$app->get('/', function (Request $request, Response $response) use ($view, $connection) {
+    $latestPosts = new LatestPosts($connection);
+    $posts = $latestPosts->get(2);
     $body = $view->render('index.twig', [
         'posts' => $posts,
     ]);
@@ -41,7 +42,7 @@ $app->get('/', function (Request $request, Response $response, $args) use ($view
     return $response;
 });
 
-$app->get('/about', function (Request $request, Response $response, $args) use ($view) {
+$app->get('/about', function (Request $request, Response $response) use ($view) {
     $body = $view->render('about.twig', [
         'name' => 'Alex',
     ]);
@@ -49,7 +50,8 @@ $app->get('/about', function (Request $request, Response $response, $args) use (
     return $response;
 });
 
-$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $postMapper) {
+$app->get('/{url_key}', function (Request $request, Response $response, $args) use ($view, $connection) {
+    $postMapper = new PostMapper($connection);
     $post = $postMapper->getByUrlKey((string) $args['url_key']);
     if (empty($post)) {
         $body = $view->render('not-found.twig');
