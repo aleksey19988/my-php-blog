2aff3e4d5f2e9d420f3753944367f0fb8a7d38ec
Aleksey Kononenko <aleksey19988@gmail.com>
2021-10-11 10:37:35 +0300

    Добавил пагинацию (логика работы пагинации ещё не реализована)

--- src/PostMapper.php
+++ src/PostMapper.php
@@ -36,16 +36,22 @@ class PostMapper
     }
 
     /**
-     * @param $direction
+     * @param int $page
+     * @param int $limit
+     * @param string $direction
      * @return array|null
      * @throws Exception
      */
-    public function getList(string $direction = 'DESC'): ?array
+    public function getList(int $page = 1, int $limit = 2, string $direction = 'DESC'): ?array
     {
         if (!in_array($direction, ['ASC', 'DESC'])) {
             throw new Exception('Некорректная сортировка в SQL-запросе!');
         }
-        $statement = $this->connection->prepare('SELECT * FROM post ORDER BY published_date ' . $direction);
+
+        $start = ($page - 1) * $limit;
+        $statement = $this->connection->prepare(
+            'SELECT * FROM post ORDER BY published_date ' . $direction . ' LIMIT ' . $start . ',' . $limit
+        );
         $statement->execute();
 
         return $statement->fetchAll();
